f9360ff36f51aaab9d3c17c3ac6a543fb79ec74e
vidhi0106 <vps43@drexel.edu>
2025-01-20 20:31:21 -0500

    new branch

---
+++ IDM362-vidhi/hwk01.swift
@@ -0,0 +1,18 @@
+//
+//  hwk01.swift
+//  IDM362-vidhi
+//
+//  Created by Vidhi Shah  on 1/20/25.
+//
+
+import SwiftUI
+
+struct hwk01: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    hwk01()
+}
