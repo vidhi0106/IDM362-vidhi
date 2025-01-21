42baeaaa4cbc878ed12ba08fc6d29064a6b78dd4
vidhi0106 <vps43@drexel.edu>
2025-01-20 20:27:01 -0500

    Initial Commit

---
+++ IDM362-vidhi/IDM362_vidhiApp.swift
@@ -0,0 +1,17 @@
+//
+//  IDM362_vidhiApp.swift
+//  IDM362-vidhi
+//
+//  Created by Vidhi Shah  on 1/20/25.
+//
+
+import SwiftUI
+
+@main
+struct IDM362_vidhiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
