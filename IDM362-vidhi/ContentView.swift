a462ba08cff0766f6d05b43797d36129913ff74c
vidhi0106 <vps43@drexel.edu>
2025-01-27 22:33:56 -0500

    chnages

--- IDM362-vidhi/ContentView.swift
+++ IDM362-vidhi/ContentView.swift
@@ -20,6 +20,7 @@ struct ContentView: View {
 //                .resizable(capInsets: EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 0.0))
 //                .foregroundColor(Color.primary)
 //                .frame(width: 60.0, height: 60.0)
+            //Spacer()
             Text("MoodCloud")
                 .frame(maxWidth:.infinity)
                 .multilineTextAlignment(.center)
