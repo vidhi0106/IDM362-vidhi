67018537a61aa70a0f9460ca099284438e5aea53
vidhi0106 <vps43@drexel.edu>
2025-02-10 22:45:57 -0500

    homework3

---
+++ IDM362-vidhi/CalendarView.swift
@@ -0,0 +1,18 @@
+//
+//  CalendarView.swift
+//  IDM362-vidhi
+//
+//  Created by Vidhi Shah  on 2/10/25.
+//
+
+import SwiftUI
+
+struct CalendarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CalendarView()
+}
---
+++ IDM362-vidhi/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  IDM362-vidhi
+//
+//  Created by Vidhi Shah  on 2/10/25.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
